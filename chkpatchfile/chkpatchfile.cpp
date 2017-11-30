2d361f4e570dc8e280fbb5f8ef7464fe6a410b74
Yd Chen (RD-TW) <yd_chen@tw.trendnet.org>
2017-11-30 18:41:27 +0800

    function added

--- chkpatchfile/chkpatchfile.cpp
+++ chkpatchfile/chkpatchfile.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string> 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ typedef struct item {
     int p4_changelist_num;
 	string submitted_by;
     int fileGroup;
-    char Type;
+    string Type;
     string destination_path;
     string source_path;
 }item;
@@ -33,7 +34,7 @@ typedef struct patchFileItem {
 }pFileItem;
 
 void WriteFile(string,std::vector<struct item>);
-
+string lowerCase(std::string);
 int _tmain(int argc, _TCHAR* argv[])
 {
     char txtName[]="./file/test.txt";
@@ -92,8 +93,8 @@ int _tmain(int argc, _TCHAR* argv[])
 			
 		}
 		string flist[3]={"./file/a.txt","./file/b.txt","./file/c.txt"};
-		for(int i=0;i<3;i++) WriteFile(flist[i],itemlist);
-		
+		//for(int i=0;i<3;i++) WriteFile(flist[i],itemlist);
+		WriteFile("./file/a.txt",itemlist);
 		
 	}
 	else cout<<"----verified----"<<endl;
@@ -108,6 +109,7 @@ void WriteFile(string filePath,std::vector<item> itemlist){
    string line;
    std::vector<pFileItem> pFileItemlist;
    rtxt.open(filePath.c_str()); //start read patchFilelist
+   cout<< "-------------------"<<endl;
    cout<< "Work on File:" << filePath << "..." <<endl;
    if(rtxt.fail()){ //if failed to open,fp=0
 		cout<<"Fail to open File:" << filePath.c_str() <<endl;
@@ -116,7 +118,9 @@ void WriteFile(string filePath,std::vector<item> itemlist){
    
    if(rtxt.is_open()){
 		getline(rtxt,line);//skip first line
+
 		while(getline(rtxt,line)){ //new line
+	
               if(line.empty()) break;
               istringstream iss(line);
 			  pFileItem readTmp;
@@ -125,35 +129,75 @@ void WriteFile(string filePath,std::vector<item> itemlist){
 					   >> readTmp.path
 					   >> readTmp.fileName
 					   >> readTmp.source_path
-					   >> readTmp.comment)
-					   pFileItemlist.push_back(readTmp);	
+					   )
+			  {
+				      //cout<<"Tmp:"<<readTmp.fileGroup <<endl;
+                      pFileItemlist.push_back(readTmp);
+			  }
+					   	
 			           //end read line		
 		}	
 	}
     rtxt.close();//close read file
+	std::vector<pFileItem>::iterator piit=pFileItemlist.begin();
 
+    cout<<"Start matching..."<<endl;
+	std::vector<item>::iterator iit=itemlist.begin();
+	int group=0;
+	
+	for(;iit!=itemlist.end();iit++){
+	  std::vector<pFileItem>::iterator piit=pFileItemlist.begin();
+	  bool fileMatchGroup=false;
+	  cout<<"----------------------"<<endl;
+	  cout<<"Now Use:"<<iit->fileName<< "  ||FileGroup:"<<iit->fileGroup<<endl;
+      for(;piit!=pFileItemlist.end();piit++){
 
-	//std::vector<item>::iterator iit=itemlist.begin();
-	//int group=1;
-	//for(;iit!=itemlist.end();iit++){
-	//  std::vector<pFileItem>::iterator piit=pFileItemlist.begin();
- //     for(;piit!=pFileItemlist.end();piit++){
-	//	  bool fileMatch=false;
-	//	  
-	//	  if((piit->fileGroup == iit->fileGroup)&&
-	//		 (piit->fileName  == iit->fileName)&&
-	//		 (piit->path  == iit->destination_path)&&
-	//		 (piit->source_path  == iit->source_path)&&
-	//		 (piit->Type  == iit->Type)) break; //Same File 
-	//	  if(group!=piit->fileGroup && fileMatch=false;){
-	//	    group=piit->fileGroup;
- //           
-	//	  } 
-	//	  
-	//  }
-	//}
+		  
+		  if((piit->fileGroup == iit->fileGroup)&&
+			 (lowerCase(piit->fileName)  == lowerCase(iit->fileName))&&
+			 (piit->path  == iit->destination_path)&&
+			 (piit->source_path  == iit->source_path)&&
+			 (piit->Type  == iit->Type)){
+				 cout<<"Find SameFile:"<<piit->fileGroup<<"||"<<piit->fileName<<endl;
+		         break;
+		  }  //Same File 
 
-
+		  if(group!=piit->fileGroup && fileMatchGroup==false && iit->fileGroup>=group){     //Next Group
+		    group=piit->fileGroup;
+			if(piit->fileGroup == iit->fileGroup){
+			  cout<<"Find SameGroup:"<<group<<"||"<<iit->fileName<<endl;
+			  fileMatchGroup=true;
+			} 
+		  } 
+		  else if(group!=piit->fileGroup && fileMatchGroup==true && iit->fileGroup>=group){
+			  cout<<"Find SameGroup and need to insert:"<<group<<"||"<<piit->fileName<<endl;
+		    //insert
+			  pFileItem tmp;
+			  tmp.fileGroup  =iit->fileGroup;
+			  tmp.fileName   =iit->fileName;
+			  tmp.path       =iit->destination_path;
+			  tmp.source_path=iit->source_path;
+			  tmp.Type       =iit->Type;
+			  tmp.comment    ="";
+			  pFileItemlist.insert(piit,tmp);
+			  break;
+		  }
+		  else if(iit->fileGroup<group||piit==(pFileItemlist.end()-1)){
+			  cout<<"Need to add file:" << iit->fileName <<" to new group:"<<iit->fileGroup<<endl;
+			  pFileItem tmp;
+			  tmp.fileGroup  =iit->fileGroup;
+			  tmp.fileName   =iit->fileName;
+			  tmp.path       =iit->destination_path;
+			  tmp.source_path=iit->source_path;
+			  tmp.Type       =iit->Type;
+			  tmp.comment    ="";
+			  pFileItemlist.insert(piit+1,tmp);
+			  break;
+		  }	  
+	  }
+	}
+    cout<<"Finished matching..."<<endl;
+    cout<<"Start to Write File..."<<endl;
     wtxt.open(filePath.c_str());//Start Write File
 	if(wtxt.fail()){ //if failed to open,fp=0
 		cout<<"Fail to open File:" << filePath.c_str() <<endl;
@@ -168,10 +212,14 @@ void WriteFile(string filePath,std::vector<item> itemlist){
 			    << piit->Type       <<"\t"
 				<< piit->path       <<"\t"
 				<< piit->fileName   <<"\t"
-				<< piit->source_path<<"\t" 
-				<< piit->comment    <<endl;
+				<< piit->source_path<<endl;
 	}
     wtxt.close();//close write file
+	cout<<"Write File End"<<endl;
 }
 
-
+string lowerCase(std::string data){
+// convert string to back to lower case
+	std::transform(data.begin(), data.end(), data.begin(), ::tolower);
+	return data;
+}
